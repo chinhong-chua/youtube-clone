a0d2fa098abaf9e680328edbeeba2c5257ba01d0
Cruz <cruz_ch96@outlook.com>
2022-08-27 23:55:37 +0800

    Initial Commit Ytb Clone

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import {Videos, ChannelCard} from './'
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const ChannelDetail = () => {
+
+  const {id} = useParams()
+
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+
+  console.log(channelDetail, videos)
+
+  useEffect(()=>{
+ fetchFromAPI(`channels?part=snippet&id=${id}`).then((data)=> setChannelDetail(data?.items[0]))
+ fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data)=> setVideos(data?.items[0]))
+
+  },[id])
+  return <div>ChannelDetail</div>;
+}; 
+
+export default ChannelDetail;
